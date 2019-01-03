c796fce16c057a9c69e8989a35bccbaf429bc828
Achal Jain <achal@ideaboxcreations.com>
2019-01-03 10:45:04 +0530

    Updated version

--- powerpack-lite-elementor.php
+++ powerpack-lite-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: PowerPack Lite for Elementor
  * Plugin URI: https://powerpackelements.com
  * Description: Custom addons for elementor page builder.
- * Version: 1.3.8
+ * Version: 1.0.0
  * Author: Team IdeaBox - PowerPack Elements
  * Author URI: http://powerpackelements.com
  * License: GNU General Public License v2.0
@@ -14,7 +14,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-define( 'POWERPACK_ELEMENTS_LITE_VER', '1.3.8' );
+define( 'POWERPACK_ELEMENTS_LITE_VER', '1.0.0' );
 define( 'POWERPACK_ELEMENTS_LITE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'POWERPACK_ELEMENTS_LITE_BASE', plugin_basename( __FILE__ ) );
 define( 'POWERPACK_ELEMENTS_LITE_URL', plugins_url( '/', __FILE__ ) );
