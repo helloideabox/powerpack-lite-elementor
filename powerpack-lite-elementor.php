8bc31f84940ed8b0e9357382b8cdea9e100ab141
--edit <simrandeepsingh@Simrandeeps-MacBook-Pro.local>
2019-10-03 16:45:47 +0530

    Added UTM links

--- powerpack-lite-elementor.php
+++ powerpack-lite-elementor.php
@@ -187,8 +187,8 @@ function pp_add_description_links( $plugin_meta, $plugin_file ) {
 
 	if ( POWERPACK_ELEMENTS_LITE_BASE === $plugin_file ) {
 		$row_meta = [
-			'docs' => '<a href="https://powerpackelements.com/docs/" aria-label="' . esc_attr( __( 'View PowerPack Documentation', 'power-pack' ) ) . '" target="_blank">' . __( 'Docs & FAQs', 'power-pack' ) . '</a>',
-			'ideo' => '<a href="https://powerpackelements.com/" aria-label="' . esc_attr( __( 'Go Pro', 'powerp-pack' ) ) . '" target="_blank" style="font-weight:bold;">' . __( 'Go Pro', 'power-pack' ) . '</a>',
+			'docs' => '<a href="https://powerpackelements.com/docs/?utm_source=doclink&utm_medium=widget&utm_campaign=lite" aria-label="' . esc_attr( __( 'View PowerPack Documentation', 'power-pack' ) ) . '" target="_blank">' . __( 'Docs & FAQs', 'power-pack' ) . '</a>',
+			'ideo' => '<a href="https://powerpackelements.com/?utm_source=plugin&utm_medium=list&utm_campaign=lite" aria-label="' . esc_attr( __( 'Go Pro', 'powerp-pack' ) ) . '" target="_blank" style="font-weight:bold;">' . __( 'Go Pro', 'power-pack' ) . '</a>',
 		];
 
 		$plugin_meta = array_merge( $plugin_meta, $row_meta );
