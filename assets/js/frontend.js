178610a5e34fbe521142733fa30cb71894344457
--edit <simrandeepsingh@Simrandeeps-MacBook-Pro.local>
2019-10-16 16:32:05 +0530

    Fixed: Slider settings were not working in Team Member Carousel widget

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -276,55 +276,9 @@
     
     var TeamMemberCarouselHandler = function ($scope, $) {
         var $carousel                   = $scope.find('.pp-tm-carousel').eq(0),
-            $items                      = ($carousel.data("items") !== undefined) ? $carousel.data("items") : 3,
-            $items_tablet               = ($carousel.data("items-tablet") !== undefined) ? $carousel.data("items-tablet") : 2,
-            $items_mobile               = ($carousel.data("items-mobile") !== undefined) ? $carousel.data("items-mobile") : 1,
-            $margin                     = ($carousel.data("margin") !== undefined) ? $carousel.data("margin") : 10,
-            $margin_tablet              = ($carousel.data("margin-tablet") !== undefined) ? $carousel.data("margin-tablet") : 10,
-            $margin_mobile              = ($carousel.data("margin-mobile") !== undefined) ? $carousel.data("margin-mobile") : 10,
-            $speed                      = ($carousel.data("speed") !== undefined) ? $carousel.data("speed") : 400,
-            $autoplay                   = ($carousel.data("autoplay") !== undefined) ? $carousel.data("autoplay") : 999999,
-            $loop                       = ($carousel.data("loop") !== undefined) ? $carousel.data("loop") : 0,
-            $grab_cursor                = ($carousel.data("grab-cursor") !== undefined) ? $carousel.data("grab-cursor") : 0,
-            $pagination                 = ($carousel.data("pagination") !== undefined) ? $carousel.data("pagination") : '.swiper-pagination',
-            $pagination_type            = ($carousel.data("pagination-type") !== undefined) ? $carousel.data("pagination-type") : 'bullets',
-            $arrows                     = ($carousel.data("arrows") !== undefined) ? $carousel.data("arrows") : false,
-            $arrow_next                 = ($carousel.data("arrow-next") !== undefined) ? $carousel.data("arrow-next") : '.swiper-button-next',
-            $arrow_prev                 = ($carousel.data("arrow-prev") !== undefined) ? $carousel.data("arrow-prev") : '.swiper-button-prev',
+            $slider_options             = JSON.parse( $carousel.attr('data-slider-settings') );
             
-            mySwiper = new Swiper($carousel, {
-                direction:              'horizontal',
-                speed:                  $speed,
-                slidesPerView:          $items,
-                spaceBetween:           $margin,
-                grabCursor:             $grab_cursor,
-                paginationClickable:    true,
-                loop:                   $loop,
-                autoplay: {
-                    delay: $autoplay,
-                },
-                pagination: {
-                    el: $pagination,
-                    type: $pagination_type,
-                    clickable: true,
-                },
-                navigation: {
-                    nextEl: $arrow_next,
-                    prevEl: $arrow_prev,
-                },
-                breakpoints: {
-                    // when window width is <= 480px
-                    480: {
-                        slidesPerView:  $items_mobile,
-                        spaceBetween:   $margin_mobile
-                    },
-                    // when window width is <= 640px
-                    768: {
-                        slidesPerView:  $items_tablet,
-                        spaceBetween:   $margin_tablet
-                    }
-                }
-            });
+        var mySwiper = new Swiper($carousel, $slider_options);
     };
     
     var ImageScrollHandler = function($scope, $) {
