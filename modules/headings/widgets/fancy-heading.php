595c76a90f3e23bca3dfe253027bfc966585c74a
Simrandeep Singh <simrandeep072@gmail.com>
2021-04-06 17:54:39 +0530

    Fancy Heading - Code Sanitization

--- modules/headings/widgets/fancy-heading.php
+++ modules/headings/widgets/fancy-heading.php
@@ -2,6 +2,8 @@
 namespace PowerpackElementsLite\Modules\Headings\Widgets;
 
 use PowerpackElementsLite\Base\Powerpack_Widget;
+use PowerpackElementsLite\Classes\PP_Helper;
+use PowerpackElementsLite\Classes\PP_Config;
 
 // Elementor Classes
 use Elementor\Controls_Manager;
@@ -76,128 +78,167 @@ class Fancy_Heading extends Powerpack_Widget {
 	 *
 	 * @access protected
 	 */
-	protected function _register_controls() {
+	protected function _register_controls() { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+		$this->register_controls();
+	}
 
+	/**
+	 * Register fancy heading widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since x.x.x
+	 * @access protected
+	 */
+	protected function register_controls() {
+		/* Content Tab */
+		$this->register_content_heading_controls();
+		$this->register_content_help_docs_controls();
+		$this->register_content_upgrade_pro_controls();
+
+		/* Style Tab */
+		$this->register_style_heading_controls();
+	}
+
+	/*-----------------------------------------------------------------------------------*/
+	/*	CONTENT TAB
+	/*-----------------------------------------------------------------------------------*/
+
+	protected function register_content_heading_controls() {
 		/**
 		 * Content Tab: Fancy Heading
 		 */
 		$this->start_controls_section(
 			'section_heading',
-			array(
-				'label' => __( 'Fancy Heading', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Fancy Heading', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'heading_text',
-			array(
-				'label'       => __( 'Heading', 'powerpack' ),
-				'type'        => Controls_Manager::TEXTAREA,
-				'dynamic'     => array(
-					'active' => true,
-				),
-				'label_block' => true,
-				'rows'        => 2,
-				'default'     => __( 'Add Your Heading Text Here', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Heading', 'powerpack' ),
+				'type'                  => Controls_Manager::TEXTAREA,
+				'dynamic'               => [
+					'active'   => true,
+				],
+				'label_block'           => true,
+				'rows'                  => 2,
+				'default'               => __( 'Add Your Heading Text Here', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'link',
-			array(
-				'label'       => __( 'Link', 'powerpack' ),
-				'type'        => Controls_Manager::URL,
-				'dynamic'     => array(
-					'active'     => true,
-					'categories' => array(
+			[
+				'label'                 => __( 'Link', 'powerpack' ),
+				'type'                  => Controls_Manager::URL,
+				'dynamic'               => [
+					'active'        => true,
+					'categories'    => [
 						TagsModule::POST_META_CATEGORY,
 						TagsModule::URL_CATEGORY,
-					),
-				),
-				'label_block' => true,
-				'placeholder' => 'https://www.your-link.com',
-			)
+					],
+				],
+				'label_block'           => true,
+				'placeholder'           => 'https://www.your-link.com',
+			]
 		);
 
 		$this->add_control(
 			'heading_html_tag',
-			array(
-				'label'       => __( 'HTML Tag', 'powerpack' ),
-				'type'        => Controls_Manager::SELECT,
-				'label_block' => false,
-				'default'     => 'h2',
-				'options'     => array(
-					'h1'   => __( 'H1', 'powerpack' ),
-					'h2'   => __( 'H2', 'powerpack' ),
-					'h3'   => __( 'H3', 'powerpack' ),
-					'h4'   => __( 'H4', 'powerpack' ),
-					'h5'   => __( 'H5', 'powerpack' ),
-					'h6'   => __( 'H6', 'powerpack' ),
-					'div'  => __( 'div', 'powerpack' ),
-					'span' => __( 'span', 'powerpack' ),
-					'p'    => __( 'p', 'powerpack' ),
-				),
-			)
+			[
+				'label'                 => __( 'HTML Tag', 'powerpack' ),
+				'type'                  => Controls_Manager::SELECT,
+				'label_block'           => false,
+				'default'               => 'h2',
+				'options'               => [
+					'h1'     => __( 'H1', 'powerpack' ),
+					'h2'     => __( 'H2', 'powerpack' ),
+					'h3'     => __( 'H3', 'powerpack' ),
+					'h4'     => __( 'H4', 'powerpack' ),
+					'h5'     => __( 'H5', 'powerpack' ),
+					'h6'     => __( 'H6', 'powerpack' ),
+					'div'    => __( 'div', 'powerpack' ),
+					'span'   => __( 'span', 'powerpack' ),
+					'p'      => __( 'p', 'powerpack' ),
+				],
+			]
 		);
 
 		$this->add_responsive_control(
 			'align',
-			array(
-				'label'       => __( 'Alignment', 'powerpack' ),
-				'type'        => Controls_Manager::CHOOSE,
-				'label_block' => false,
-				'options'     => array(
-					'left'    => array(
+			[
+				'label'                 => __( 'Alignment', 'powerpack' ),
+				'type'                  => Controls_Manager::CHOOSE,
+				'label_block'           => false,
+				'options'               => [
+					'left' => [
 						'title' => __( 'Left', 'powerpack' ),
-						'icon'  => 'eicon-text-align-left',
-					),
-					'center'  => array(
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
 						'title' => __( 'Center', 'powerpack' ),
-						'icon'  => 'eicon-text-align-center',
-					),
-					'right'   => array(
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
 						'title' => __( 'Right', 'powerpack' ),
-						'icon'  => 'eicon-text-align-right',
-					),
-					'justify' => array(
+						'icon' => 'eicon-text-align-right',
+					],
+					'justify' => [
 						'title' => __( 'Justified', 'powerpack' ),
-						'icon'  => 'eicon-text-align-justify',
-					),
-				),
-				'default'     => '',
-				'selectors'   => array(
-					'{{WRAPPER}}' => 'text-align: {{VALUE}};',
-				),
-			)
+						'icon' => 'eicon-text-align-justify',
+					],
+				],
+				'default'               => '',
+				'selectors'             => [
+					'{{WRAPPER}}'   => 'text-align: {{VALUE}};',
+				],
+			]
 		);
 
 		$this->end_controls_section();
+	}
 
-		/**
-		 * Content Tab: Help Docs
-		 *
-		 * @since 1.4.8
-		 * @access protected
-		 */
-		$this->start_controls_section(
-			'section_help_docs',
-			array(
-				'label' => __( 'Help Docs', 'powerpack' ),
-			)
-		);
+	protected function register_content_help_docs_controls() {
 
-		$this->add_control(
-			'help_doc_1',
-			array(
-				'type'            => Controls_Manager::RAW_HTML,
-				/* translators: %1$s doc link */
-				'raw'             => sprintf( __( '%1$s Watch Video Overview %2$s', 'powerpack' ), '<a href="https://www.youtube.com/watch?v=PxWWUTeW4dc&list=PLpsSO_wNe8Dz4vfe2tWlySBCCFEgh1qZj" target="_blank" rel="noopener">', '</a>' ),
-				'content_classes' => 'pp-editor-doc-links',
-			)
-		);
+		$help_docs = PP_Config::get_widget_help_links( 'Fancy_Heading' );
 
-		$this->end_controls_section();
+		if ( ! empty( $help_docs ) ) {
 
+			/**
+			 * Content Tab: Help Docs
+			 *
+			 * @since 1.4.8
+			 * @access protected
+			 */
+			$this->start_controls_section(
+				'section_help_docs',
+				[
+					'label' => __( 'Help Docs', 'powerpack' ),
+				]
+			);
+
+			$hd_counter = 1;
+			foreach ( $help_docs as $hd_title => $hd_link ) {
+				$this->add_control(
+					'help_doc_' . $hd_counter,
+					[
+						'type'            => Controls_Manager::RAW_HTML,
+						'raw'             => sprintf( '%1$s ' . $hd_title . ' %2$s', '<a href="' . $hd_link . '" target="_blank" rel="noopener">', '</a>' ),
+						'content_classes' => 'pp-editor-doc-links',
+					]
+				);
+
+				$hd_counter++;
+			}
+
+			$this->end_controls_section();
+		}
+	}
+
+	protected function register_content_upgrade_pro_controls() {
 		if ( ! is_pp_elements_active() ) {
 			/**
 			 * Content Tab: Upgrade PowerPack
@@ -225,87 +266,87 @@ class Fancy_Heading extends Powerpack_Widget {
 
 			$this->end_controls_section();
 		}
+	}
 
-		/*
-		-----------------------------------------------------------------------------------*/
-		/*
-		  STYLE TAB
-		/*-----------------------------------------------------------------------------------*/
+	/*-----------------------------------------------------------------------------------*/
+	/*	STYLE TAB
+	/*-----------------------------------------------------------------------------------*/
 
+	protected function register_style_heading_controls() {
 		/**
 		 * Style Tab: First Part
 		 */
 		$this->start_controls_section(
-			'first_section_style',
-			array(
-				'label' => __( 'Fancy Heading', 'powerpack' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
-			)
+			'heading_section_style',
+			[
+				'label'                 => __( 'Fancy Heading', 'powerpack' ),
+				'tab'                   => Controls_Manager::TAB_STYLE,
+			]
 		);
 
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
-			array(
-				'name'     => 'typography',
-				'label'    => __( 'Typography', 'powerpack' ),
-				'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
-				'selector' => '{{WRAPPER}} .pp-heading-text',
-			)
+			[
+				'name'                  => 'typography',
+				'label'                 => __( 'Typography', 'powerpack' ),
+				'scheme'                => Scheme_Typography::TYPOGRAPHY_1,
+				'selector'              => '{{WRAPPER}} .pp-heading-text',
+			]
 		);
 
 		$this->add_group_control(
 			Group_Control_Text_Shadow::get_type(),
-			array(
-				'name'     => 'heading_text_shadow',
-				'selector' => '{{WRAPPER}} .pp-heading-text',
-			)
+			[
+				'name'                  => 'heading_text_shadow',
+				'selector'              => '{{WRAPPER}} .pp-heading-text',
+			]
 		);
 
 		$this->add_control(
 			'heading_fill',
-			array(
-				'label'        => __( 'Fill', 'powerpack' ),
-				'type'         => Controls_Manager::SELECT,
-				'options'      => array(
-					'solid'    => __( 'Color', 'powerpack' ),
-					'gradient' => __( 'Background', 'powerpack' ),
-				),
-				'default'      => 'solid',
-				'prefix_class' => 'pp-heading-fill-',
-				'separator'    => 'before',
-			)
+			[
+				'label'                 => __( 'Fill', 'powerpack' ),
+				'type'                  => Controls_Manager::SELECT,
+				'options'               => [
+					'solid'     => __( 'Color', 'powerpack' ),
+					'gradient'  => __( 'Background', 'powerpack' ),
+				],
+				'default'               => 'solid',
+				'prefix_class'          => 'pp-heading-fill-',
+				'separator'             => 'before',
+			]
 		);
 
 		$this->add_group_control(
 			Group_Control_Background::get_type(),
-			array(
-				'name'      => 'gradient',
-				'types'     => array( 'gradient', 'classic' ),
-				'selector'  => '{{WRAPPER}} .pp-heading-text',
-				'default'   => 'gradient',
-				'condition' => array(
-					'heading_fill' => 'gradient',
-				),
-			)
+			[
+				'name'                  => 'gradient',
+				'types'                 => [ 'gradient', 'classic' ],
+				'selector'              => '{{WRAPPER}} .pp-heading-text',
+				'default'               => 'gradient',
+				'condition'             => [
+					'heading_fill'  => 'gradient',
+				],
+			]
 		);
 
 		$this->add_control(
 			'heading_text_color',
-			array(
-				'label'     => __( 'Text Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'scheme'    => array(
+			[
+				'label'                 => __( 'Text Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'scheme'                => [
 					'type'  => Scheme_Color::get_type(),
 					'value' => Scheme_Color::COLOR_1,
-				),
-				'default'   => '',
-				'selectors' => array(
+				],
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} .pp-heading-text' => 'color: {{VALUE}};',
-				),
-				'condition' => array(
+				],
+				'condition'             => [
 					'heading_fill' => 'solid',
-				),
-			)
+				],
+			]
 		);
 
 		$this->end_controls_section();
@@ -326,20 +367,29 @@ class Fancy_Heading extends Powerpack_Widget {
 		$this->add_inline_editing_attributes( 'heading_text', 'basic' );
 		$this->add_render_attribute( 'heading_text', 'class', 'pp-heading-text' );
 
-		if ( ! empty( $settings['link']['url'] ) ) {
+		if ( '' !== $settings['link']['url'] ) {
 			$this->add_link_attributes( 'fancy-heading-link', $settings['link'] );
 		}
 
 		if ( $settings['heading_text'] ) {
-			printf( '<%1$s %2$s>', $settings['heading_html_tag'], $this->get_render_attribute_string( 'fancy-heading' ) );
-			if ( ! empty( $settings['link']['url'] ) ) {
-				printf( '<a %1$s>', $this->get_render_attribute_string( 'fancy-heading-link' ) ); }
-
-				printf( '<span %1$s>%2$s</span>', $this->get_render_attribute_string( 'heading_text' ), $this->parse_text_editor( $settings['heading_text'] ) );
-
-			if ( ! empty( $settings['link']['url'] ) ) {
-				printf( '</a>' ); }
-			printf( '</%1$s>', $settings['heading_html_tag'] );
+			$heading_html_tag = PP_Helper::validate_html_tag( $settings['heading_html_tag'] );
+			?>
+			<<?php echo esc_html( $heading_html_tag ); ?> <?php echo wp_kses_post( $this->get_render_attribute_string( 'fancy-heading' ) ); ?>>
+				<?php
+				if ( '' !== $settings['link']['url'] ) { ?>
+					<a <?php echo wp_kses_post( $this->get_render_attribute_string( 'fancy-heading-link' ) ); ?>>
+					<?php
+				}
+				?>
+				<span <?php echo $this->get_render_attribute_string( 'heading_text' ); ?>>
+					<?php echo $this->parse_text_editor( $settings['heading_text'] ); ?>
+				</span>
+				<?php
+				if ( '' !== $settings['link']['url'] ) { ?>
+					</a>
+				<?php } ?>
+			</<?php echo esc_html( $heading_html_tag ); ?>>
+			<?php
 		}
 	}
 
@@ -350,7 +400,7 @@ class Fancy_Heading extends Powerpack_Widget {
 	 *
 	 * @access protected
 	 */
-	protected function _content_template() {
+	protected function content_template() {
 		?>
 		<{{{settings.heading_html_tag}}} class="pp-fancy-heading">
 			<# if ( settings.link.url ) { #><a href="{{settings.link.url}}"><# } #>
@@ -370,5 +420,19 @@ class Fancy_Heading extends Powerpack_Widget {
 			<# if ( settings.link.url ) { #></a><# } #>
 		</{{{settings.heading_html_tag}}}>
 		<?php
+	}
+
+	/**
+	 * Render fancy heading widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * Remove this after Elementor v3.3.0
+	 *
+	 * @since 1.2.6
+	 * @access protected
+	 */
+	protected function _content_template() { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+		$this->content_template();
 	}
 }
