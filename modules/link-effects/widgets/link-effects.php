4fb8807a3ffeed0d79513ff5d8052e9856f59352
Simrandeep Singh <simrandeep072@gmail.com>
2021-04-07 10:26:30 +0530

    Link Effects - Code Sanitization

--- modules/link-effects/widgets/link-effects.php
+++ modules/link-effects/widgets/link-effects.php
@@ -10,8 +10,8 @@ use Elementor\Repeater;
 use Elementor\Group_Control_Background;
 use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Typography;
-use Elementor\Scheme_Typography;
-use Elementor\Scheme_Color;
+use Elementor\Core\Schemes\Typography as Scheme_Typography;
+use Elementor\Core\Schemes\Color as Scheme_Color;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -76,12 +76,22 @@ class Link_Effects extends Powerpack_Widget {
 	 *
 	 * @access protected
 	 */
-	protected function _register_controls() {
+	protected function _register_controls() { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+		$this->register_controls();
+	}
 
-		/*
-		-----------------------------------------------------------------------------------*/
-		/*
-		  CONTENT TAB
+	/**
+	 * Register link effects widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since x.x.x
+	 * @access protected
+	 */
+	protected function register_controls() {
+
+		/*-----------------------------------------------------------------------------------*/
+		/*	CONTENT TAB
 		/*-----------------------------------------------------------------------------------*/
 
 		/**
@@ -90,60 +100,60 @@ class Link_Effects extends Powerpack_Widget {
 		 */
 		$this->start_controls_section(
 			'section_link_effects',
-			array(
-				'label' => __( 'Link Effects', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Link Effects', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'text',
-			array(
-				'label'       => __( 'Text', 'powerpack' ),
-				'type'        => Controls_Manager::TEXT,
-				'label_block' => true,
-				'dynamic'     => array(
-					'active' => true,
-				),
-				'default'     => __( 'Click Here', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Text', 'powerpack' ),
+				'type'                  => Controls_Manager::TEXT,
+				'label_block'           => true,
+				'dynamic'               => [
+					'active'   => true,
+				],
+				'default'               => __( 'Click Here', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'secondary_text',
-			array(
-				'label'     => __( 'Secondary Text', 'powerpack' ),
-				'type'      => Controls_Manager::TEXT,
-				'dynamic'   => array(
-					'active' => true,
-				),
-				'default'   => __( 'Click Here', 'powerpack' ),
-				'condition' => array(
-					'effect' => 'effect-9',
-				),
-			)
+			[
+				'label'                 => __( 'Secondary Text', 'powerpack' ),
+				'type'                  => Controls_Manager::TEXT,
+				'dynamic'               => [
+					'active'   => true,
+				],
+				'default'               => __( 'Click Here', 'powerpack' ),
+				'condition'             => [
+					'effect'    => 'effect-9',
+				],
+			]
 		);
 
 		$this->add_control(
 			'link',
-			array(
-				'label'       => __( 'Link', 'powerpack' ),
-				'type'        => Controls_Manager::URL,
-				'dynamic'     => array(
-					'active' => true,
-				),
-				'placeholder' => 'https://www.your-link.com',
-				'default'     => array(
+			[
+				'label'                 => __( 'Link', 'powerpack' ),
+				'type'                  => Controls_Manager::URL,
+				'dynamic'               => [
+					'active'   => true,
+				],
+				'placeholder'           => 'https://www.your-link.com',
+				'default'               => [
 					'url' => '#',
-				),
-			)
+				],
+			]
 		);
 
 		$this->add_control(
 			'effect',
-			array(
-				'label'   => __( 'Effect', 'powerpack' ),
-				'type'    => Controls_Manager::SELECT,
-				'options' => array(
+			[
+				'label'                 => __( 'Effect', 'powerpack' ),
+				'type'                  => Controls_Manager::SELECT,
+				'options'               => [
 					'effect-1'  => __( 'Bottom Border Slides In', 'powerpack' ),
 					'effect-2'  => __( 'Bottom Border Slides Out', 'powerpack' ),
 					'effect-3'  => __( 'Brackets', 'powerpack' ),
@@ -165,39 +175,39 @@ class Link_Effects extends Powerpack_Widget {
 					'effect-19' => __( '3D Side', 'powerpack' ),
 					'effect-20' => __( 'Unfold', 'powerpack' ),
 					'effect-21' => __( 'Borders Slight Yranslate', 'powerpack' ),
-				),
-				'default' => 'effect-1',
-			)
+				],
+				'default'               => 'effect-1',
+			]
 		);
 
 		$this->add_responsive_control(
 			'align',
-			array(
-				'label'     => __( 'Alignment', 'powerpack' ),
-				'type'      => Controls_Manager::CHOOSE,
-				'options'   => array(
-					'left'    => array(
+			[
+				'label'                 => __( 'Alignment', 'powerpack' ),
+				'type'                  => Controls_Manager::CHOOSE,
+				'options'               => [
+					'left'      => [
 						'title' => __( 'Left', 'powerpack' ),
 						'icon'  => 'fa fa-align-left',
-					),
-					'center'  => array(
+					],
+					'center'    => [
 						'title' => __( 'Center', 'powerpack' ),
 						'icon'  => 'fa fa-align-center',
-					),
-					'right'   => array(
+					],
+					'right'     => [
 						'title' => __( 'Right', 'powerpack' ),
 						'icon'  => 'fa fa-align-right',
-					),
-					'justify' => array(
+					],
+					'justify'   => [
 						'title' => __( 'Justified', 'powerpack' ),
 						'icon'  => 'fa fa-align-justify',
-					),
-				),
-				'default'   => '',
-				'selectors' => array(
-					'{{WRAPPER}}' => 'text-align: {{VALUE}};',
-				),
-			)
+					],
+				],
+				'default'               => '',
+				'selectors'             => [
+					'{{WRAPPER}}'   => 'text-align: {{VALUE}};',
+				],
+			]
 		);
 
 		$this->end_controls_section();
@@ -229,10 +239,8 @@ class Link_Effects extends Powerpack_Widget {
 			$this->end_controls_section();
 		}
 
-		/*
-		-----------------------------------------------------------------------------------*/
-		/*
-		  STYLE TAB
+		/*-----------------------------------------------------------------------------------*/
+		/*	STYLE TAB
 		/*-----------------------------------------------------------------------------------*/
 
 		/**
@@ -241,144 +249,144 @@ class Link_Effects extends Powerpack_Widget {
 		 */
 		$this->start_controls_section(
 			'section_style',
-			array(
-				'label' => __( 'Link Effects', 'powerpack' ),
-				'tab'   => Controls_Manager::TAB_STYLE,
-			)
+			[
+				'label'                 => __( 'Link Effects', 'powerpack' ),
+				'tab'                   => Controls_Manager::TAB_STYLE,
+			]
 		);
 
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
-			array(
-				'name'     => 'typography',
-				'label'    => __( 'Typography', 'powerpack' ),
-				'scheme'   => Scheme_Typography::TYPOGRAPHY_4,
-				'selector' => '{{WRAPPER}} a.pp-link',
-			)
+			[
+				'name'                  => 'typography',
+				'label'                 => __( 'Typography', 'powerpack' ),
+				'scheme'                => Scheme_Typography::TYPOGRAPHY_4,
+				'selector'              => '{{WRAPPER}} a.pp-link',
+			]
 		);
 
 		$this->add_responsive_control(
 			'divider_title_width',
-			array(
-				'label'      => __( 'Width', 'powerpack' ),
-				'type'       => Controls_Manager::SLIDER,
-				'default'    => array(
-					'size' => 200,
-				),
-				'range'      => array(
-					'px' => array(
-						'min'  => 1,
-						'max'  => 1000,
-						'step' => 1,
-					),
-				),
-				'size_units' => array( 'px', '%' ),
-				'selectors'  => array(
-					'{{WRAPPER}} .pp-link-effect-19'      => 'width: {{SIZE}}{{UNIT}}',
+			[
+				'label'                 => __( 'Width', 'powerpack' ),
+				'type'                  => Controls_Manager::SLIDER,
+				'default'               => [
+					'size'  => 200,
+				],
+				'range'                 => [
+					'px' => [
+						'min'   => 1,
+						'max'   => 1000,
+						'step'  => 1,
+					],
+				],
+				'size_units'            => [ 'px', '%' ],
+				'selectors'             => [
+					'{{WRAPPER}} .pp-link-effect-19' => 'width: {{SIZE}}{{UNIT}}',
 					'{{WRAPPER}} .pp-link-effect-19 span' => 'transform-origin: 50% 50% calc(-{{SIZE}}{{UNIT}}/2)',
-				),
-				'condition'  => array(
+				],
+				'condition'             => [
 					'effect' => 'effect-19',
-				),
-			)
+				],
+			]
 		);
 
 		$this->start_controls_tabs( 'tabs_link_style' );
 
 		$this->start_controls_tab(
 			'tab_link_normal',
-			array(
-				'label' => __( 'Normal', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Normal', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'link_color_normal',
-			array(
-				'label'     => __( 'Link Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Link Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} a.pp-link, {{WRAPPER}} .pp-link-effect-10 span, {{WRAPPER}} .pp-link-effect-15:before, {{WRAPPER}} .pp-link-effect-16, {{WRAPPER}} .pp-link-effect-17:before' => 'color: {{VALUE}};',
-				),
-			)
+				],
+			]
 		);
 
 		$this->add_control(
 			'background_color_normal',
-			array(
-				'label'     => __( 'Background Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Background Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} .pp-link-effect-4 span, {{WRAPPER}} .pp-link-effect-10 span, {{WRAPPER}} .pp-link-effect-19 span, {{WRAPPER}} .pp-link-effect-20 span' => 'background-color: {{VALUE}};',
-				),
-			)
+				],
+			]
 		);
 
 		$this->add_control(
 			'link_border_color',
-			array(
-				'label'     => __( 'Border Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Border Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} .pp-link-effect-8:before' => 'border-color: {{VALUE}};',
-					'{{WRAPPER}} .pp-link-effect-11'       => 'border-top-color: {{VALUE}};',
+					'{{WRAPPER}} .pp-link-effect-11' => 'border-top-color: {{VALUE}};',
 					'{{WRAPPER}} .pp-link-effect-1:after, {{WRAPPER}} .pp-link-effect-2:after, {{WRAPPER}} .pp-link-effect-6:before, {{WRAPPER}} .pp-link-effect-6:after, {{WRAPPER}} .pp-link-effect-7:before, {{WRAPPER}} .pp-link-effect-7:after, {{WRAPPER}} .pp-link-effect-14:before, {{WRAPPER}} .pp-link-effect-14:after, {{WRAPPER}} .pp-link-effect-18:before, {{WRAPPER}} .pp-link-effect-18:after' => 'background: {{VALUE}};',
 					'{{WRAPPER}} .pp-link-effect-3:before, {{WRAPPER}} .pp-link-effect-3:after' => 'color: {{VALUE}};',
-					'{{WRAPPER}} .pp-link-effect-20 span'  => 'box-shadow: inset 0 3px {{VALUE}};',
-				),
-			)
+					'{{WRAPPER}} .pp-link-effect-20 span' => 'box-shadow: inset 0 3px {{VALUE}};',
+				],
+			]
 		);
 
 		$this->end_controls_tab();
 
 		$this->start_controls_tab(
 			'tab_link_hover',
-			array(
-				'label' => __( 'Hover', 'powerpack' ),
-			)
+			[
+				'label'                 => __( 'Hover', 'powerpack' ),
+			]
 		);
 
 		$this->add_control(
 			'link_color_hover',
-			array(
-				'label'     => __( 'Link Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Link Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} a.pp-link:hover, {{WRAPPER}} .pp-link-effect-10:before, {{WRAPPER}} .pp-link-effect-11:before, {{WRAPPER}} .pp-link-effect-15, {{WRAPPER}} .pp-link-effect-16:before, {{WRAPPER}} .pp-link-effect-20 span:before' => 'color: {{VALUE}};',
-				),
-			)
+				],
+			]
 		);
 
 		$this->add_control(
 			'background_color_hover',
-			array(
-				'label'     => __( 'Background Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Background Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} .pp-link-effect-4 span:before, {{WRAPPER}} .pp-link-effect-10:before, {{WRAPPER}} .pp-link-effect-19 span:before, {{WRAPPER}} .pp-link-effect-20 span:before' => 'background-color: {{VALUE}};',
-				),
-			)
+				],
+			]
 		);
 
 		$this->add_control(
 			'link_border_color_hover',
-			array(
-				'label'     => __( 'Border Color', 'powerpack' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => array(
+			[
+				'label'                 => __( 'Border Color', 'powerpack' ),
+				'type'                  => Controls_Manager::COLOR,
+				'default'               => '',
+				'selectors'             => [
 					'{{WRAPPER}} .pp-link-effect-8:after' => 'border-color: {{VALUE}};',
 					'{{WRAPPER}} .pp-link-effect-11:before' => 'border-bottom-color: {{VALUE}};',
 					'{{WRAPPER}} .pp-link-effect-9:before, {{WRAPPER}} .pp-link-effect-9:after, {{WRAPPER}} .pp-link-effect-14:hover:before, {{WRAPPER}} .pp-link-effect-14:focus:before, {{WRAPPER}} .pp-link-effect-14:hover:after, {{WRAPPER}} .pp-link-effect-14:focus:after, {{WRAPPER}} .pp-link-effect-17:after, {{WRAPPER}} .pp-link-effect-18:hover:before, {{WRAPPER}} .pp-link-effect-18:focus:before, {{WRAPPER}} .pp-link-effect-18:hover:after, {{WRAPPER}} .pp-link-effect-18:focus:after, {{WRAPPER}} .pp-link-effect-21:before, {{WRAPPER}} .pp-link-effect-21:after' => 'background: {{VALUE}};',
-					'{{WRAPPER}} .pp-link-effect-17'      => 'color: {{VALUE}};',
+					'{{WRAPPER}} .pp-link-effect-17' => 'color: {{VALUE}};',
 					'{{WRAPPER}} .pp-link-effect-13:hover:before, {{WRAPPER}} .pp-link-effect-13:focus:before' => 'color: {{VALUE}}; text-shadow: 10px 0 {{VALUE}}, -10px 0 {{VALUE}}',
-				),
-			)
+				],
+			]
 		);
 
 		$this->end_controls_tab();
@@ -397,10 +405,8 @@ class Link_Effects extends Powerpack_Widget {
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 
-		// get our input from the widget settings.
-
-		$pa_link_text           = ! empty( $settings['text'] ) ? $settings['text'] : '';
-		$pa_link_secondary_text = ! empty( $settings['secondary_text'] ) ? $settings['secondary_text'] : '';
+		$link_text = '' !== $settings['text'] ? $settings['text'] : '';
+		$link_secondary_text = ! empty( $settings['secondary_text'] ) ? $settings['secondary_text'] : '';
 
 		if ( ! empty( $settings['link']['url'] ) ) {
 			$this->add_link_attributes( 'link', $settings['link'] );
@@ -412,27 +418,37 @@ class Link_Effects extends Powerpack_Widget {
 			$this->add_render_attribute( 'link', 'class', 'pp-link-' . $settings['effect'] );
 		}
 
-		if ( $settings['effect'] == 'effect-4' || $settings['effect'] == 'effect-5' || $settings['effect'] == 'effect-19' || $settings['effect'] == 'effect-20' ) {
-			$this->add_render_attribute( 'pp-link-text', 'data-hover', $pa_link_text );
-		}
+		switch ( $settings['effect'] ) {
+			case 'effect-4':
+			case 'effect-5':
+			case 'effect-19':
+			case 'effect-20':
+				$this->add_render_attribute( 'pp-link-text', 'data-hover', $link_text );
+				break;
 
-		if ( $settings['effect'] == 'effect-10' || $settings['effect'] == 'effect-11' || $settings['effect'] == 'effect-15' || $settings['effect'] == 'effect-16' || $settings['effect'] == 'effect-17' || $settings['effect'] == 'effect-18' ) {
-			$this->add_render_attribute( 'pp-link-text-2', 'data-hover', $pa_link_text );
+			case 'effect-10':
+			case 'effect-11':
+			case 'effect-15':
+			case 'effect-16':
+			case 'effect-17':
+			case 'effect-18':
+				$this->add_render_attribute( 'pp-link-text-2', 'data-hover', $link_text );
+				break;
 		}
 		?>
 
-		<a <?php echo $this->get_render_attribute_string( 'link' ); ?><?php echo $this->get_render_attribute_string( 'pp-link-text-2' ); ?>>
-			<span <?php echo $this->get_render_attribute_string( 'pp-link-text' ); ?>>
-				<?php echo $pa_link_text; ?>
+		<a <?php echo wp_kses_post( $this->get_render_attribute_string( 'link' ) ); ?> <?php echo wp_kses_post( $this->get_render_attribute_string( 'pp-link-text-2' ) ); ?>>
+			<span <?php echo wp_kses_post( $this->get_render_attribute_string( 'pp-link-text' ) ); ?>>
+				<?php echo esc_attr( $link_text ); ?>
 			</span>
-			<?php if ( $settings['effect'] == 'effect-9' ) { ?>
-				<span><?php echo esc_attr( $pa_link_secondary_text ); ?></span>
+			<?php if ( 'effect-9' === $settings['effect'] ) { ?>
+				<span><?php echo esc_attr( $link_secondary_text ); ?></span>
 			<?php } ?>
 		</a>
-
 		<?php
 	}
 
-	protected function _content_template() {}
+	protected function _content_template() { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+	}
 
 }
