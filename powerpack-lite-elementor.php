81fb5529e72cc5a677c937901503ba7c14ba76cb
Simrandeep Singh <simrandeep072@gmail.com>
2023-01-18 11:14:50 +0530

    Updated Elementor compatibility tags

--- powerpack-lite-elementor.php
+++ powerpack-lite-elementor.php
@@ -10,8 +10,8 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: powerpack
  * Domain Path: /languages
- * Elementor tested up to: 3.9.0
- * Elementor Pro tested up to: 3.9.0
+ * Elementor tested up to: 3.10.1
+ * Elementor Pro tested up to: 3.10.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
