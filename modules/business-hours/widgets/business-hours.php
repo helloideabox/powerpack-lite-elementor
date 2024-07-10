fe0cf6297e24562e4227d254632c6fb13c981843
Simrandeep Singh <simrandeep072@gmail.com>
2024-07-10 17:56:03 +0530

    Business Hours - Fixed translation issue with predefined days

--- modules/business-hours/widgets/business-hours.php
+++ modules/business-hours/widgets/business-hours.php
@@ -117,57 +117,57 @@ class Business_Hours extends Powerpack_Widget {
 			)
 		);
 
-		$pp_hours = array(
-			'00:00' => '12:00 AM',
-			'00:30' => '12:30 AM',
-			'01:00' => '1:00 AM',
-			'01:30' => '1:30 AM',
-			'02:00' => '2:00 AM',
-			'02:30' => '2:30 AM',
-			'03:00' => '3:00 AM',
-			'03:30' => '3:30 AM',
-			'04:00' => '4:00 AM',
-			'04:30' => '4:30 AM',
-			'05:00' => '5:00 AM',
-			'05:30' => '5:30 AM',
-			'06:00' => '6:00 AM',
-			'06:30' => '6:30 AM',
-			'07:00' => '7:00 AM',
-			'07:30' => '7:30 AM',
-			'08:00' => '8:00 AM',
-			'08:30' => '8:30 AM',
-			'09:00' => '9:00 AM',
-			'09:30' => '9:30 AM',
-			'10:00' => '10:00 AM',
-			'10:30' => '10:30 AM',
-			'11:00' => '11:00 AM',
-			'11:30' => '11:30 AM',
-			'12:00' => '12:00 PM',
-			'12:30' => '12:30 PM',
-			'13:00' => '1:00 PM',
-			'13:30' => '1:30 PM',
-			'14:00' => '2:00 PM',
-			'14:30' => '2:30 PM',
-			'15:00' => '3:00 PM',
-			'15:30' => '3:30 PM',
-			'16:00' => '4:00 PM',
-			'16:30' => '4:30 PM',
-			'17:00' => '5:00 PM',
-			'17:30' => '5:30 PM',
-			'18:00' => '6:00 PM',
-			'18:30' => '6:30 PM',
-			'19:00' => '7:00 PM',
-			'19:30' => '7:30 PM',
-			'20:00' => '8:00 PM',
-			'20:30' => '8:30 PM',
-			'21:00' => '9:00 PM',
-			'21:30' => '9:30 PM',
-			'22:00' => '10:00 PM',
-			'22:30' => '10:30 PM',
-			'23:00' => '11:00 PM',
-			'23:30' => '11:30 PM',
-			'24:00' => '12:00 PM',
-			'24:30' => '12:30 PM',
+		$hours = array(
+			'00:00' => __( '12:00 AM', 'powerpack' ),
+			'00:30' => __( '12:30 AM', 'powerpack' ),
+			'01:00' => __( '1:00 AM', 'powerpack' ),
+			'01:30' => __( '1:30 AM', 'powerpack' ),
+			'02:00' => __( '2:00 AM', 'powerpack' ),
+			'02:30' => __( '2:30 AM', 'powerpack' ),
+			'03:00' => __( '3:00 AM', 'powerpack' ),
+			'03:30' => __( '3:30 AM', 'powerpack' ),
+			'04:00' => __( '4:00 AM', 'powerpack' ),
+			'04:30' => __( '4:30 AM', 'powerpack' ),
+			'05:00' => __( '5:00 AM', 'powerpack' ),
+			'05:30' => __( '5:30 AM', 'powerpack' ),
+			'06:00' => __( '6:00 AM', 'powerpack' ),
+			'06:30' => __( '6:30 AM', 'powerpack' ),
+			'07:00' => __( '7:00 AM', 'powerpack' ),
+			'07:30' => __( '7:30 AM', 'powerpack' ),
+			'08:00' => __( '8:00 AM', 'powerpack' ),
+			'08:30' => __( '8:30 AM', 'powerpack' ),
+			'09:00' => __( '9:00 AM', 'powerpack' ),
+			'09:30' => __( '9:30 AM', 'powerpack' ),
+			'10:00' => __( '10:00 AM', 'powerpack' ),
+			'10:30' => __( '10:30 AM', 'powerpack' ),
+			'11:00' => __( '11:00 AM', 'powerpack' ),
+			'11:30' => __( '11:30 AM', 'powerpack' ),
+			'12:00' => __( '12:00 PM', 'powerpack' ),
+			'12:30' => __( '12:30 PM', 'powerpack' ),
+			'13:00' => __( '1:00 PM', 'powerpack' ),
+			'13:30' => __( '1:30 PM', 'powerpack' ),
+			'14:00' => __( '2:00 PM', 'powerpack' ),
+			'14:30' => __( '2:30 PM', 'powerpack' ),
+			'15:00' => __( '3:00 PM', 'powerpack' ),
+			'15:30' => __( '3:30 PM', 'powerpack' ),
+			'16:00' => __( '4:00 PM', 'powerpack' ),
+			'16:30' => __( '4:30 PM', 'powerpack' ),
+			'17:00' => __( '5:00 PM', 'powerpack' ),
+			'17:30' => __( '5:30 PM', 'powerpack' ),
+			'18:00' => __( '6:00 PM', 'powerpack' ),
+			'18:30' => __( '6:30 PM', 'powerpack' ),
+			'19:00' => __( '7:00 PM', 'powerpack' ),
+			'19:30' => __( '7:30 PM', 'powerpack' ),
+			'20:00' => __( '8:00 PM', 'powerpack' ),
+			'20:30' => __( '8:30 PM', 'powerpack' ),
+			'21:00' => __( '9:00 PM', 'powerpack' ),
+			'21:30' => __( '9:30 PM', 'powerpack' ),
+			'22:00' => __( '10:00 PM', 'powerpack' ),
+			'22:30' => __( '10:30 PM', 'powerpack' ),
+			'23:00' => __( '11:00 PM', 'powerpack' ),
+			'23:30' => __( '11:30 PM', 'powerpack' ),
+			'24:00' => __( '12:00 PM', 'powerpack' ),
+			'24:30' => __( '12:30 PM', 'powerpack' ),
 		);
 
 		$repeater = new Repeater();
@@ -208,7 +208,7 @@ class Business_Hours extends Powerpack_Widget {
 				'label'     => __( 'Opening Hours', 'powerpack' ),
 				'type'      => Controls_Manager::SELECT,
 				'default'   => '09:00',
-				'options'   => $pp_hours,
+				'options'   => $hours,
 				'condition' => array(
 					'closed' => 'no',
 				),
@@ -221,7 +221,7 @@ class Business_Hours extends Powerpack_Widget {
 				'label'     => __( 'Closing Hours', 'powerpack' ),
 				'type'      => Controls_Manager::SELECT,
 				'default'   => '17:00',
-				'options'   => $pp_hours,
+				'options'   => $hours,
 				'condition' => array(
 					'closed' => 'no',
 				),
@@ -1068,6 +1068,27 @@ class Business_Hours extends Powerpack_Widget {
 	}
 
 	/**
+	 * Get predefined days
+	 *
+	 * @access protected
+	 */
+	protected function get_predefined_days( $day ) {
+		$days = array(
+			'Monday'    => __( 'Monday', 'powerpack' ),
+			'Tuesday'   => __( 'Tuesday', 'powerpack' ),
+			'Wednesday' => __( 'Wednesday', 'powerpack' ),
+			'Thursday'  => __( 'Thursday', 'powerpack' ),
+			'Friday'    => __( 'Friday', 'powerpack' ),
+			'Saturday'  => __( 'Saturday', 'powerpack' ),
+			'Sunday'    => __( 'Sunday', 'powerpack' ),
+		);
+
+		if ( ! empty( $day ) ) {
+			return $days[ $day ];
+		}
+	}
+
+	/**
 	 * Render predefined Business Hours widget output on the frontend.
 	 *
 	 * Written in PHP and used to generate the final HTML.
@@ -1094,7 +1115,7 @@ class Business_Hours extends Powerpack_Widget {
 				<span class="pp-business-day">
 					<?php
 					if ( 'long' === $settings['days_format'] ) {
-						echo esc_attr( ucwords( $item['day'] ) );
+						echo esc_attr( ucwords( $this->get_predefined_days( $item['day'] ) ) );
 					} else {
 						echo esc_attr( ucwords( substr( $item['day'], 0, 3 ) ) );
 					}
@@ -1172,7 +1193,7 @@ class Business_Hours extends Powerpack_Widget {
 							echo esc_attr( $item['time'] );
 						} else {
 							if ( $item['closed_text'] ) {
-								$this->print_unescaped_setting( 'closed_text', 'business_hours', $index );
+								$this->print_unescaped_setting( 'closed_text', 'business_hours_custom', $index );
 							} else {
 								esc_attr_e( 'Closed', 'powerpack' );
 							}
