4f25e5e47d494bd586114b778e411774ce8c2706
Simrandeep Singh <simrandeep072@gmail.com>
2026-02-11 07:38:16 +0530

    Updated Elementor comparison version

--- classes/class-pp-admin-settings.php
+++ classes/class-pp-admin-settings.php
@@ -145,7 +145,7 @@ final class PP_Admin_Settings {
 		$func        = __CLASS__ . '::render';
 
 		if ( current_user_can( 'manage_options' ) ) {
-			if ( version_compare( ELEMENTOR_VERSION, '3.35.0-beta1', '>=' ) ) {
+			if ( version_compare( ELEMENTOR_VERSION, '3.35.0', '>=' ) ) {
 				add_submenu_page( 'elementor-home', $title, $title, $cap, $slug, $func );
 			} else {
 				add_submenu_page( 'elementor', $title, $title, $cap, $slug, $func );
