079028b8bb7114d6efb74e8ab68d419c572f7e47
Simrandeep Singh <simrandeep072@gmail.com>
2026-09-18 06:59:37 +0530

    Updated minimum Elementor ver required to 4.0.0

--- powerpack-lite-elementor.php
+++ powerpack-lite-elementor.php
@@ -54,7 +54,7 @@ define( 'POWERPACK_ELEMENTS_LITE_VER', '3.1.0' );
 define( 'POWERPACK_ELEMENTS_LITE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'POWERPACK_ELEMENTS_LITE_BASE', plugin_basename( __FILE__ ) );
 define( 'POWERPACK_ELEMENTS_LITE_URL', plugins_url( '/', __FILE__ ) );
-define( 'POWERPACK_ELEMENTS_LITE_ELEMENTOR_VERSION_REQUIRED', '3.5.0' );
+define( 'POWERPACK_ELEMENTS_LITE_ELEMENTOR_VERSION_REQUIRED', '4.0.0' );
 define( 'POWERPACK_ELEMENTS_LITE_PHP_VERSION_REQUIRED', '7.4' );
 
 require_once POWERPACK_ELEMENTS_LITE_PATH . 'includes/helper-functions.php';
